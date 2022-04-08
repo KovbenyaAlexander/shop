81a7e54bce382994dad8241d7930ae1e3c2ea149
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2022-04-08 21:36:51 +0300

    add styles for contacts section

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+// import {
+//   faFacebook, faInstagram, faTelegram, faTwitter,
+// } from '@fortawesome/free-brands-svg-icons';
 import './style.scss';
 
 const Contacts = (): JSX.Element => {
@@ -9,14 +14,47 @@ const Contacts = (): JSX.Element => {
 
       <div className="container">
         <h2>Контакты</h2>
-        <p>Наш адрес:</p>
-        <p>г. Киев, ул. Космонавтов, д. 7, офис 5</p>
-        <p>Наша почта</p>
-        <p>aaaa@gmail.com</p>
-        <button type="button">забронировать стол</button>
-        <p>+7 (999) 999-99-99</p>
-        <p>Звоните или оставляйте заявку</p>
-        <span>Мы в соц сетях:</span>
+
+        <hr className="contacts__hr" />
+
+        <div className="contacts__adress">
+          <FontAwesomeIcon data-isbtn="true" icon={faLocationDot} className="contacts__icon" />
+          <div>
+            <p>Наш адрес:</p>
+            <p>г. Киев, ул. Космонавтов, д. 7, офис 5</p>
+          </div>
+        </div>
+
+        <div className="contacts__email">
+          <FontAwesomeIcon data-isbtn="true" icon={faEnvelope} className="contacts__icon" />
+          <div>
+            <p>Наша почта</p>
+            <p>aaaa@gmail.com</p>
+          </div>
+
+        </div>
+
+        <hr className="contacts__hr" />
+
+        <div className="contacts__phone-container">
+          <button className="contacts__btn" type="button">забронировать стол</button>
+          <div>
+            <p className="contacts__phone">+7 (999) 999-99-99</p>
+            <p>Звоните или оставляйте заявку</p>
+          </div>
+
+        </div>
+
+        <hr className="contacts__hr" />
+
+        <div className="contacts__social">
+          <span>Мы в соц сетях:</span>
+          {/* <FontAwesomeIcon data-isbtn="true" icon={faFacebook} className="contacts__icon" />
+        <FontAwesomeIcon data-isbtn="true" icon={faInstagram} className="contacts__icon" />
+        <FontAwesomeIcon data-isbtn="true" icon={faTelegram} className="contacts__icon" />
+        <FontAwesomeIcon data-isbtn="true" icon={faTwitter} className="contacts__icon" /> */}
+        </div>
+
       </div>
     </section>
 
