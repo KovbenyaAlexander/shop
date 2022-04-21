616111953f027f610e6ff38bb0d39ce4972deb89
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2022-04-21 23:00:22 +0300

    add styles for Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
