aa9248641d5192f314f2aa8360618355e7c291b3
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2022-11-08 21:18:16 +0300

    deploy

--- client/README.md
+++
@@ -1,1 +0,0 @@
-# [Deploy](https://kovbenyaalexander.github.io/shop/#/)
--- client/src/services/UserService.js
+++
@@ -1,7 +0,0 @@
-import api from "../axios/axios";
-
-export default class UserService {
-  static async getUsers() {
-    return api.get("/users");
-  }
-}
